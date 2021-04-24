be516eb4ba6c4cc11da9c24836065bca750888f4
Chitransh Jain <cjsmcs2810@gmail.com>
2021-04-24 13:24:32 +0530

    admin route, login done

---
+++ src/Admin/AdminAuth.js
@@ -0,0 +1,14 @@
+import { reactLocalStorage } from "reactjs-localstorage";
+
+function isAuth() {
+    const admin = reactLocalStorage.getObject("tripshrip-admin");
+    if(admin.role === "admin") {
+        console.log("ADMIN ACCESS GRANTED");
+        return true;
+    } else {
+        console.log("ADMIN ACCESS DENIED");
+        return false;
+    }
+}
+
+export default isAuth;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Local imports
 import Home from "./Core/Home";
-import { gsap } from "gsap";
-import Header from './Components/Header'
-import Footer from './Components/Footer'
 import Aboutus from "./Core/Aboutus";
-import ContactUs from "./Core/ContactUs"
+import ContactUs from "./Core/ContactUs";
+import AdminLogin from "./Admin/AdminLogin";
+
+import AdminRoute from "./Admin/AdminRoute";
+import AdminDashboard from "./Admin/AdminDashboard";
 
 function App() {
   return (
     <div className="App" style={{ width: "100%", overflowX: "hidden" }}>
       <Router>
-        <Header />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/aboutus" exact component={Aboutus} />
-          <Route path="/contact-us" exact component={ContactUs}/>
+          <Route path="/contact-us" exact component={ContactUs} />
+          <Route path="/admin/login" exact component={AdminLogin} />
+          <AdminRoute
+            path="/admin/dashboard"
+            exact
+            component={AdminDashboard}
+          />
         </Switch>
-        <Footer />
       </Router>
     </div>
   );
