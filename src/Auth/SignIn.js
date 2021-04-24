714d91fadbb9a7eba5a37bc349ab3ea043bf50f1
Chitransh Jain <cjsmcs2810@gmail.com>
2021-04-24 23:33:57 +0530

    handle change functions done

--- src/Auth/SignIn.js
+++ src/Auth/SignIn.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./Auth.css";
 
 function SignIn() {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setCredentials((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
   return (
     <div className="auth-main-div">
       <Card className="auth-card">
@@ -12,15 +22,29 @@ function SignIn() {
           <h4>Sign In</h4>
           <Form className="auth-form">
             <Form.Group>
-              <Form.Control type="email" name="email" className="auth-input" placeholder="Email"/>
+              <Form.Control
+                type="email"
+                name="email"
+                className="auth-input"
+                placeholder="Email"
+                value={credentials.email}
+                onChange={handleChange}
+              />
             </Form.Group>
             <Form.Group>
-              <Form.Control type="password" name="password" className="auth-input" placeholder="Password"/>
+              <Form.Control
+                type="password"
+                name="password"
+                className="auth-input"
+                placeholder="Password"
+                value={credentials.password}
+                onChange={handleChange}
+              />
             </Form.Group>
             <Button className="auth-button">Sign In</Button>
           </Form>
           <div className="auth-text">
-              New here? <Link to="/signup">Sign Up</Link>
+            New here? <Link to="/signup">Sign Up</Link>
           </div>
         </Card.Body>
       </Card>
--- src/Auth/SignUp.js
+++ src/Auth/SignUp.js
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./Auth.css";
 
 function SignUp() {
-    return (
-      <div className="auth-main-div">
-        <Card className="auth-card">
-          <Card.Body>
-            <h4>Sign Up</h4>
-            <Form className="auth-form">
-              <Form.Group>
-                <Form.Control type="email" name="email" className="auth-input" placeholder="Email"/>
-              </Form.Group>
-              <Form.Group>
-                <Form.Control type="password" name="password" className="auth-input" placeholder="Password"/>
-              </Form.Group>
-              <Button className="auth-button">Sign Up</Button>
-            </Form>
-            <div className="auth-text">
-                Already Registered? <Link to="/signin">Sign In</Link>
-            </div>
-          </Card.Body>
-        </Card>
-      </div>
-    );
-  }
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setCredentials((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  return (
+    <div className="auth-main-div">
+      <Card className="auth-card">
+        <Card.Body>
+          <h4>Sign Up</h4>
+          <Form className="auth-form">
+            <Form.Group>
+              <Form.Control
+                type="email"
+                name="email"
+                className="auth-input"
+                placeholder="Email"
+                value={credentials.email}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="password"
+                name="password"
+                className="auth-input"
+                placeholder="Password"
+                value={credentials.password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button className="auth-button">Sign Up</Button>
+          </Form>
+          <div className="auth-text">
+            Already Registered? <Link to="/signin">Sign In</Link>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
 
-export default SignUp
+export default SignUp;
