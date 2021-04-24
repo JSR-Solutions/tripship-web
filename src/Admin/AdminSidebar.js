578ab0aaf5d4eaaab61efe3ca556f600d366a19f
Chitransh Jain <cjsmcs2810@gmail.com>
2021-04-24 14:54:16 +0530

    sidebar effect change

--- src/Admin/AdminSidebar.js
+++ src/Admin/AdminSidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { slide as Menu } from "react-burger-menu";
+import { bubble as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 
 //Local imports
